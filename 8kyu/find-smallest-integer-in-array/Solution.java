c28fb50d3659dbb623960ace1bd4e702d977c698
Vyshnavi <109726262+46172443@users.noreply.github.com>
2026-06-15 16:07:35 +0530

    Update Solution.java

--- 8kyu/find-smallest-integer-in-array/Solution.java
+++ 8kyu/find-smallest-integer-in-array/Solution.java
@@ -1,6 +1,11 @@
-public class Kata {
-    public static int findsmallestintegerinarray(int number) {
-        // TODO: implement
-        return int;
+public class SmallestIntegerFinder {
+    public static int findSmallestInt(int[] args) {
+      int min = args[0];
+      for(int num:args){
+        if(num<min){
+          min = num;
+        }
+      }
+        return min;
     }
 }
