b5b756e2fc9fa2f1daf368443bb8e292a2a15236
Vyshnavi <vyshnavisankati27@gmail.com>
2026-06-16 20:33:00 +0530

    Solve 8kyu invert values

--- 8kyu/invert-values/Solution.java
+++ 8kyu/invert-values/Solution.java
@@ -1,6 +1,7 @@
 public class Kata {
     public static int invertvalues(int number) {
-        // TODO: implement
-        return int;
+        for (int i = 0; i < array.length; i++) {
+            array[i] = -array[i];
+        } return array;
     }
 }
